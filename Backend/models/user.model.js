09e0d61a651a9548f03765b33078219bc558054c
dhruv-1006 <dhruv.bairagi27@gmail.com>
2026-05-13 15:52:03 +0530

    Add user login route

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -30,13 +30,6 @@ const userSchema = new mongoose.Schema({
     },
 })    
 
-// userSchema.methods.generateAuthToken = function() {
-//     const token = jwt.sign(
-//         { _id: this._id }, 
-//         process.env.JWT_SECRET);
-//     return token;
-// };
-
 userSchema.methods.generateAuthToken = function () {
 
     return jwt.sign(
