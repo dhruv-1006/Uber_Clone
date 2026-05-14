133dac69ce9506149151b307a4894e9b7503ddf4
dhruv-1006 <dhruv.bairagi27@gmail.com>
2026-05-14 16:53:20 +0530

    Register captain done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -13,6 +13,12 @@ module.exports.registerUser = async (req, res, next) => {
 
     const { fullname, email, password } = req.body;
 
+    const isUserAlreadyExist = await userModel.findOne({email});
+
+    if (isUserAlreadyExist) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
     const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
@@ -60,7 +66,7 @@ module.exports.getUserProfile = async (req, res, next) => {
 module.exports.logoutUser = async (req, res, next) => {
     res.clearCookie('token');
     const token = req.cokkies.token || req.headers.authorization.split(' ')[1];
-    
+
     await blaacklistTokenModel.create({ token });
 
     res.status(200).json({ message: 'Logged out' });
