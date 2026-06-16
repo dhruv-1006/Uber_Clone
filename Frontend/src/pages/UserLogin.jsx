b574546e64a349369c0beae1ac4f04aded20c8d0
dhruv-1006 <dhruv.bairagi27@gmail.com>
2026-06-16 15:52:58 +0530

    Setup User Login Page

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const UserLogin = () => {
   return (
     <div className='p-7'>
+
+      <div>
+        <img className='w-16 mb-10'src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
       <form action=" ">
-        <h3 className='text-xl mb-2'>What's your phone number or email ?</h3>
+        <h3 className='text-lg font-medium mb-2'>What's your phone number or email ?</h3>
 
         <input required
         className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' 
@@ -12,15 +15,28 @@ const UserLogin = () => {
         placeholder='Phone number or email'
         />
 
-        <h3 className='text-xl mb-2'>Enter Password</h3>
+        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
 
         <input required 
+        className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
         type="password" 
         placeholder='Password'
         />
         
-        <button>Login</button>
+        <button
+        className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base'
+        >Login</button>
       </form>
+      </div>
+      
+      <div>
+        <button
+        className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base'
+        >
+          Sign in as captain
+        </button>
+      </div>  
+
     </div>
   )
 }
