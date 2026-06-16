e559711f3b9df4bbf75ca614e2bcbcc101f32603
dhruv-1006 <dhruv.bairagi27@gmail.com>
2026-06-16 15:27:24 +0530

    Setup User Login Page

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className='p-7'>
+      <form action=" ">
+        <h3 className='text-xl mb-2'>What's your phone number or email ?</h3>
+
+        <input required
+        className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' 
+        type="text" 
+        placeholder='Phone number or email'
+        />
+
+        <h3 className='text-xl mb-2'>Enter Password</h3>
+
+        <input required 
+        type="password" 
+        placeholder='Password'
+        />
+        
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
